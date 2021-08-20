3b9b7dce61fef843ec96aab6e84258ebf3c52d87
rekhanaveen <rekammu@gmail.com>
2021-08-20 19:51:00 +0530

    Added:Employee Partime Salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,20 @@
 echo "Welcome to Employee Wage Computation"
 isPresent=0
 isAbsent=1
-randomNumber=$((RANDOM%2))
+isPartime=2
+randomNumber=$((RANDOM%3))
 empHr=0
 salaryPerHr=20
 if [ $randomNumber -eq $isPresent ]
 then empHr=8
 echo "Employee is Present"
+elif [ $randomNumber -eq $isPartime ]
+then
+ empHr=4
+echo "Employee is Partime"
 else
-echo "Employee is Absent"
+ empHr=0
+	echo "Employee is Absent"
 fi
 totalSalary=$((salaryPerHr*empHr))
 echo "Salary:-" $totalSalary
