637901f294cbb17887204711bce75ff646c35087
rekhanaveen <rekammu@gmail.com>
2021-08-20 19:23:17 +0530

    Added:Code of Employee fullday salary calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,9 +2,13 @@ echo "Welcome to Employee Wage Computation"
 isPresent=0
 isAbsent=1
 randomNumber=$((RANDOM%2))
+empHr=0
+salaryPerHr=20
 if [ $randomNumber -eq $isPresent ]
-then
+then empHr=8
 echo "Employee is Present"
 else
 echo "Employee is Absent"
 fi
+totalSalary=$((salaryPerHr*empHr))
+echo "Salary:-" $totalSalary
